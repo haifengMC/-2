e4f9aa7f01c5bd1823ae7b35f3ba75ef8ba14cb7
haifengMC <1745502795@qq.com>
2019-08-13 10:21:13 +0800

    启用为守护进程

--- 07day/02启动为守护进程/src/main.cpp
+++ 07day/02启动为守护进程/src/main.cpp
@@ -3,8 +3,39 @@
 
 using namespace std;
 
+void daemon()
+{
+	int pid = fork();
+	switch(pid)
+	{
+	case 1:
+		exit(-1);
+	case 0:
+		{
+			int fd = open("/dev/null", O_RDWR);
+
+			if (fd > 0)
+			{
+				setsid();
+				
+				dup2(fd, STDIN_FILENO);
+				dup2(fd, STDOUT_FILENO);
+				dup2(fd, STDERR_FILENO);
+			}
+
+			close(fd);
+		}
+		break;
+	default:
+		exit(0);
+	}
+}
+
+
 int main()
 {
+	daemon();
+
 	if (ZinxKernel::ZinxKernelInit())
 	{
 		Ichannel* p_gameC = new ZinxTCPListen(8080, new GameCF);
